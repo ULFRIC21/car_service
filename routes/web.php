5b97478fd6987f8f57857197a34d84e411fcf7ed
ULFRIC21 <grigorkokovihin@gmail.com>
2026-05-18 16:43:43 +0400

    пока просто изменения
    делается на данный момент обычный скелет админки

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth', 'admin'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::get('/', [App\Http\Controllers\Admin\Dashboard__controller::class, 'index'])->name('dashboard');
+    });
+    // вот тут я написал Dashboard__controller: ему не понравилось а дык как App\Http\Controllers\Admin\ то все окей почему?
